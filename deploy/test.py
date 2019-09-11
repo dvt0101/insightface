dddf67b32909a58477e0df958c90c091f968365d
VietThang <vietthangtik15@gmail.com>
2019-09-11 13:56:25 +0700

    test

--- deploy/test.py
+++ deploy/test.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser(description='face model test')
 # general
 parser.add_argument('--image-size', default='112,112', help='')
 parser.add_argument('--model', default='/content/insightface/checkpoint/model-0000.params', help='path to load model.')
-parser.add_argument('--ga-model', default='', help='path to load model.')
+parser.add_argument('--ga-model', default='/content/insightface/checkpoint/model-0000.params', help='path to load model.')
 parser.add_argument('--gpu', default=0, type=int, help='gpu id')
 parser.add_argument('--det', default=0, type=int, help='mtcnn option, 1 means using R+O, 0 means detect from begining')
 parser.add_argument('--flip', default=0, type=int, help='whether do lr flip aug')
