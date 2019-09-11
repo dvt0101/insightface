37fcd4dd359575a7f14ccab132605868bcc071b0
VietThang <vietthangtik15@gmail.com>
2019-09-11 13:54:15 +0700

    test

--- deploy/test.py
+++ deploy/test.py
@@ -17,6 +17,7 @@ args = parser.parse_args()
 
 model = face_model.FaceModel(args)
 img = cv2.imread('/content/insightface/datasets/sample1.jpg')
+print(img.shape)
 img = model.get_input(img)
 f1 = model.get_feature(img)
 print(f1[0:10])
